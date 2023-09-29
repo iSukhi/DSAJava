138f6d2ad9f80dc4129ed957ef814acec9299e3a
vaibhav <vaibhav.sukhdeve@in.panasonic.com>
2023-09-29 23:10:12 +0530

    XORSum = Success

--- src/main/java/com/isukhi/dsa/Main.java
+++ src/main/java/com/isukhi/dsa/Main.java
@@ -3,12 +3,12 @@ package com.isukhi.dsa;
 import com.isukhi.dsa.bit_manupulation.AnyBaseToDecimal;
 import com.isukhi.dsa.bit_manupulation.DecimalToAnyBase;
 import com.isukhi.dsa.bit_manupulation.SingleNumber;
+import com.isukhi.dsa.bit_manupulation.XORSum;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
     }
 }
